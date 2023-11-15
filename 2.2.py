90b7ac24e14304feee8cb1a080b55fcf7fc2ae51
BelalG99 <148985158+BelalG99@users.noreply.github.com>
2023-11-15 18:18:40 +0100

    Update 2.2.py

--- 2.2.py
+++ 2.2.py
@@ -1,13 +1,13 @@
 def average(numbers):
     if not numbers:
-        return 0  # Return 0 if the sequence is empty
+        return 0  
     
     total = sum(numbers)
     num_count = len(numbers)
     avg = total / num_count
-    return int(avg)  # Returning integer average
+    return int(avg)  
 
-# Sample usage
-print(average([1, 4, 4]))  # Output: 3
-print(average([4, 4, 4, 4]))  # Output: 4
-print(average([-2, 2]))  # Output: 0
+
+print(average([1, 4, 4]))  
+print(average([4, 4, 4, 4]))  
+print(average([-2, 2]))  
