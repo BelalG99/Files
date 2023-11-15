47fe8836dd91e362d27aa5c812f8051a5b8fb5af
BelalG99 <148985158+BelalG99@users.noreply.github.com>
2023-11-15 18:22:00 +0100

    Update 2.3.py

--- 2.3.py
+++ 2.3.py
@@ -2,7 +2,7 @@ def sum_of_ints(seq1, seq2):
     total_sum = sum(seq1) + sum(seq2)
     return total_sum
 
-# Sample usage
-print(sum_of_ints([1, 4, 2], [2, -1]))  # Output: 8
-print(sum_of_ints([4, 4, 4, 4], []))  # Output: 16
-print(sum_of_ints([4, 4], [4, 4]))    # Output: 16
+
+print(sum_of_ints([1, 4, 2], [2, -1]))  
+print(sum_of_ints([4, 4, 4, 4], []))  
+print(sum_of_ints([4, 4], [4, 4]))   
